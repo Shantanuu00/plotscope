f664dc9879bdded02e9b94b03cc8d6a21b63fac2
Shantanu Pawar <106537844+Shantanuu00@users.noreply.github.com>
2026-03-17 17:51:38 +0530

    Fix Import Image picker trigger in PlotScope shell

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as PropertiesPanel } from "./PropertiesPanel";
+export { default as ToolSidebar } from "./ToolSidebar";
+export { default as TopBar } from "./TopBar";
+export { default as WorkspaceCanvas } from "./WorkspaceCanvas";
